a1e76dff66ef8e1687d5c66178e89b96b0293165
Rushikesh Patil <rnpatil2001aug@gmail.com>
2022-04-06 00:13:43 +0530

    Fixed declarations in defs.h and types.h

--- defs.h
+++ defs.h
@@ -9,6 +9,7 @@ struct spinlock;
 struct sleeplock;
 struct stat;
 struct superblock;
+struct sigaction;
 
 // bio.c
 void            binit(void);
--- types.h
+++ types.h
@@ -7,4 +7,4 @@ struct sigaction
 {
   void (*sa_handler)(int);
   uint sigmask;
-}
+};
